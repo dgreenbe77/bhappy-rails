ebc28319d80d59e172f206d3240005fd7199cbf2
Daniel Greenberg <dgreenbe777@gmail.com>
2014-04-27 18:53:28 -0400

    added fade in and fade to transistion to form page :

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
@@ -9,6 +10,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
   alias_method :facebook, :all
   alias_method :twitter, :all
+  
 end
